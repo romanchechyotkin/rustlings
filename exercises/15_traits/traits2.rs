ce3c11c395e10ef1babe9124ee36de47368d1e4f
romanchechyotkin <romanchechyotkin@gmail.com>
2024-01-20 17:14:42 +0300

    errors generics traits qiuz3

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -8,13 +8,17 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+   fn append_bar(self) -> Self {
+       let mut cloned_vec = self.clone();
+       cloned_vec.push(String::from("Bar"));
+       cloned_vec
+   } 
+}
 
 #[cfg(test)]
 mod tests {
